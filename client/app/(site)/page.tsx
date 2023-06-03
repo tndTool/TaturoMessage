8abe698a8fc633d115fb0e3d899f75779ca38b66
I0ntdtn0I <19522362@gm.uit.edu.vn>
2023-06-03 12:33:07 +0700

    upadte

--- client/app/(site)/page.tsx
+++ client/app/(site)/page.tsx
@@ -18,7 +18,9 @@ export default function Home() {
         >
             <div className="sm:mx-auto sm:w-full sm:max-w-md">
                 <Image alt="Logo" height="48" width="48" className="mx-auto w-auto" src="/images/logo.png" />
-                <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Taturo's Messenger</h2>
+                <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+                    Taturo&apos;s Messenger
+                </h2>
             </div>
 
             {/* AuthForm */}
