1649124532b018a405b361f9a1d8af332fa44ce8
I0ntdtn0I <19522362@gm.uit.edu.vn>
2023-06-03 12:37:39 +0700

    upadte

--- client/app/components/sidebar/MobileFooter.tsx
+++ client/app/components/sidebar/MobileFooter.tsx
@@ -1,9 +1,8 @@
 'use client';
 
-import MobileItem from './MobileItem';
-
-import useRoutes from '@/app/hooks/useRoutes';
 import useConversation from '@/app/hooks/useConversation';
+import useRoutes from '@/app/hooks/useRoutes';
+import MobileItem from './MobileItem';
 
 const MobileFooter = () => {
     const routes = useRoutes();
@@ -12,15 +11,29 @@ const MobileFooter = () => {
     if (isOpen) {
         return null;
     }
+
     return (
-        <div className="fixed justify-between w-full bottom-0 z-40 flex items-center bg-white border-t-[1px] lg:hidden">
+        <div
+            className="
+        fixed 
+        justify-between 
+        w-full 
+        bottom-0 
+        z-40 
+        flex 
+        items-center 
+        bg-white 
+        border-t-[1px] 
+        lg:hidden
+      "
+        >
             {routes.map((route) => (
                 <MobileItem
                     key={route.href}
                     href={route.href}
                     active={route.active}
                     icon={route.icon}
-                    onCLick={route.onClick}
+                    onClick={route.onClick}
                 />
             ))}
         </div>
